2ab9f824e2e02749a0b70445d53e533a0f91f766
drussey <drussey@trialpay.com>
2021-02-08 09:00:38 -0800

    improve grammar and formatting

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -10,9 +10,10 @@ module.exports = {
     `I'm a software engineer using critical thinking and problem solving to develop
     backend application logic. I enjoy learning new technologies to add to my toolbox 
     for creating reliable backend software and infastructure for web sofware that can
-    scale to handle consumer demand. I have experience in working creating and maintaining
-    infastructure on cloud computing platforms and detecting and mitigating live production
-    errors with both infastructure and application code.`,
+    scale to handle consumer demand. I have experience in creating and maintaining
+    infastructure on cloud computing platforms, AWS in particular. These resopnsibilities
+    also include detecting and mitigating live production errors with both infastructure 
+    and application code fixes.`,
   skills: [
     {
       name: 'PHP',
@@ -58,14 +59,14 @@ module.exports = {
       location: 'San Francisco, CA',
       occupation: 'Backend Software Engineer',
       description:
-        'At TrialPay.com / Visa I work with to create backend PHP code utilizing MySQL and memcached\
-        to provide APIs and dynamic webpages to give users access to card-linked offers. I also\
-        work with a variety of AWS technologies including EC2 instances, load balancers, S3 storage,\
-        and the Cloudfront CDN to ensure site reliability on live production servers.\
-        I also work with the Spring Framework, particularly Spring Boot as well as MySQL and kafka\
-        to develop other backend APIs for the Visa Crypto team. For this team I have researched and\
-        explored the functions of Ethereum smart contracts and work with Solidity to create proof of\
-        concept smart contracts for Visa.',
+        `At TrialPay.com / Visa I work with to create backend PHP code utilizing MySQL and memcached
+        to provide APIs and dynamic webpages to give users access to card-linked offers. I also
+        work with a variety of AWS technologies including EC2 instances, load balancers, S3 storage,
+        and the Cloudfront CDN to ensure site reliability on live production servers.
+        I also work with the Spring Framework, particularly Spring Boot as well as MySQL and kafka
+        to develop other backend APIs for the Visa Crypto team. For this team I have researched and
+        explored the functions of Ethereum smart contracts and work with Solidity to create proof of
+        concept smart contracts for Visa.`,
     },
     {
       company: 'Northrop Grumman',
@@ -77,12 +78,12 @@ module.exports = {
       location: 'Irving, TX',
       occupation: 'Software Engineer',
       description:
-        'At Northrop Grumman I worked as a developer to create a Smalltalk based application that\
-        utilized an Oracle database to store and display manufacturing instructions. I developed new\
-        modules within the application and bug fixes to support maintenance of the application. I also\
-        utilized Oracle PL/SQL to develop scheduled stored procedures to do routine data processing.\
-        Similarly, I created PL/SQL stored procedures that migrated data from legacy systems into the\
-        newer manufacturing application.',
+        `At Northrop Grumman I worked as a developer to create a Smalltalk based application that
+        utilized an Oracle database to store and display manufacturing instructions. I developed new
+        modules within the application and bug fixes to support maintenance of the application. I also
+        utilized Oracle PL/SQL to develop scheduled stored procedures to do routine data processing.
+        Similarly, I created PL/SQL stored procedures that migrated data from legacy systems into the
+        newer manufacturing application.`,
     },
   ],
   education: [
